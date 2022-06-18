bb759e805311d3e508da12d917855c8e53c256e8
Aisharya28071995 <gaishwarya58@gmail.com>
2022-06-18 16:02:31 +0530

    Commiting welcome message

---
+++ AddressBook/AddressBook/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class AddressBook
+    {
+        public static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to Address Book AddressBookMain class on Master Branch");
+        }
+    }
+}
